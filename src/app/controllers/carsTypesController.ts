f9c4f0ed8221f9eb30f312a5647250afce1e5796
gabriel <gabrieuvelezmoro@gmail.com>
2024-07-13 19:55:15 -0300

    feat: acha todos os carros dos modelos

--- src/app/controllers/carsTypesController.ts
+++ src/app/controllers/carsTypesController.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
 import CarTypes from '@app/app/models/carTypes';
+import Car from '@app/app/models/cars';
 import { AppError } from '@app/errors/app-error';
 class CarTypesController {
   public async index(req: Request, res: Response) {
     const carTypes = await CarTypes.findAndCountAll({
       order: ['description'],
     });
+
+    for (let index = 0; index < carTypes.rows.length; index++) {
+      await Car.findAll({
+        where: { carTypeId: carTypes.rows[index].id },
+      }).then(cars => {
+        carTypes.rows[index].dataValues.cars = cars;
+      });
+    }
 
     return res.json({
       data: carTypes.rows,
